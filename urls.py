b043b4bf58afbecfdc677dfe8301635706e05b42
Dominique Guardiola <contact@quinode.fr>
2011-11-22 12:22:13 +0100

    sys

--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
+import sys
 
 #from autocomplete.views import autocomplete
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
